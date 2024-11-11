d78d1c97b007296b9e62bb8a1e6b5f2dac661832
jmoreano574 <jmoreano@unal.edu.co>
2024-11-11 07:57:31 -0500

    Update Motor.java

--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -15,7 +15,7 @@ public class Motor {
         } else if (argumento.equals("gasolina")) {
             this.tipo = argumento;
         } else {
-    
+            this.tipo = tipo;
         }
     }
 }
