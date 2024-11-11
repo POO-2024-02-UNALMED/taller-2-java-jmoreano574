c4f43d4b2cd02d61b3d9fd8046e6030e104d78fe
jmoreano574 <jmoreano@unal.edu.co>
2024-11-11 14:35:54 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,6 +1,6 @@
 package test;
-
-public class Auto {
+public class Auto 
+{
     String modelo;
     int precio;
     Asiento[] asientos;
@@ -8,46 +8,18 @@ public class Auto {
     Motor motor;
     int registro;
 
-    public int cantidadAsientos() {
+    int cantidadAsientos() 
+    {
         int contador = 0;  
 
-        for (Asiento asiento : asientos) {
-            if (asiento != null) { 
+        for (Asiento i : asientos) 
+        {
+            if (i != null) 
+            { 
                 contador++;
             }
         }
 
         return contador;  
-    }
-
-    public boolean verificarIntegridad() {
-    
-        if (motor == null || motor.registro != registro) {
-            return false;
-        }
-
-        for (Asiento asiento : asientos) {
-            if (asiento != null && asiento.registro != registro) {
-                return false;
-            }
-        }
-
-        return true; 
-    }
-}
-
-class Asiento {
-    int registro;
-
-    public Asiento(int registro) {
-        this.registro = registro;
-    }
-}
-
-class Motor {
-    int registro;
-
-    public Motor(int registro) {
-        this.registro = registro;
     }
 }
