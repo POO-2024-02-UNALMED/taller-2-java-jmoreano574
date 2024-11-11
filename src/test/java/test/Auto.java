afb99921c24a9d861a3282d4736f3973b35d4f9d
jmoreano574 <jmoreano@unal.edu.co>
2024-11-11 15:01:36 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,6 +1,4 @@
-package test;
-public class Auto 
-{
+public class Auto {
     String modelo;
     int precio;
     Asiento[] asientos;
@@ -8,18 +6,31 @@ public class Auto
     Motor motor;
     int registro;
 
-    int cantidadAsientos() 
-    {
-        int contador = 0;  
+    int cantidadAsientos() {
+        int contador = 0;
 
-        for (Asiento i : asientos) 
-        {
-            if (i != null) 
-            { 
-                contador++;
+        if (asientos != null) {
+            for (Asiento asiento : asientos) {
+                if (asiento != null) { 
+                    contador++;
+                }
             }
         }
 
         return contador;  
+    }
+
+    String verificarIntegridad() 
+    {
+        if (motor == null || motor.registro != registro) {
+            return "Las piezas no son originales";
+        }
+
+        for (Asiento i : asientos) {
+            if (i != null && i.registro != registro) {
+                return "Las piezas no son originales";
+            }
+        }
+        return "Auto original";
     }
 }
